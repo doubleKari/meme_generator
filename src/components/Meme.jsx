a3d437acc9d48ffdf1dd0d89cf1a41b22ec3a57b
Yaw Karikari Wiafe <kaykayyaw@gmail.com>
2022-12-20 01:58:33 +0000

    Add placeholder text

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -5,12 +5,12 @@ const Meme = () => {
   return (
     <form action="" className="flex flex-col items-center gap-4">
       <div className="flex-col space-y-5 md:space-y-0 md:flex md:flex-row gap-5">
-        <input type="text" className="inputs" />
+        <input type="text" className="inputs" placeholder="Top text" />
 
-        <input type="text" className="inputs" />
+        <input type="text" className="inputs" placeholder="Bottom text" />
       </div>
 
-      <button className="block bg-gradient-to-r from-[#711F8D] to-[#A818DA] h-[40px] w-[230px] md:h-12 md:w-[660px] rounded-md">
+      <button className="block bg-gradient-to-r from-[#711F8D] to-[#A818DA] h-[45px] w-[230px] md:h-12 md:w-[660px] rounded-md">
         <img
           src={newMeme}
           alt="Button text"
